b8ee83ce77529aea6ff46610efd6d7f710959bf1
programmergeek <programergeek@gmail.com>
2020-08-23 12:46:26 +0200

    Created weather component

---
+++ daily-starter/src/components/Weather/Weather.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Weather.css'
+
+// Connects to weather API to get current weather data
+class Weather extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Weather
