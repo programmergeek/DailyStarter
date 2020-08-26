71d2f6d4cbd2755d181043f6d131a39d7225c893
programmergeek <programergeek@gmail.com>
2020-08-26 11:31:25 +0200

    Created state useCelcius

--- daily-starter/src/components/Weather/Weather.js
+++ daily-starter/src/components/Weather/Weather.js
@@ -4,6 +4,9 @@ import './Weather.css'
 class Weather extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            useCelcius: true,
+        }
     }
 
     render(){
