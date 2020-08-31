5490b0d03c31229ba361048f7efb2478c42f9460
programmergeek <programergeek@gmail.com>
2020-08-31 16:51:11 +0200

    Created News component

---
+++ daily-starter/src/components/News/News.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import axios from 'axios'
+
+class News extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default News
