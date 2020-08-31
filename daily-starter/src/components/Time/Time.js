eacdaeb3fd681014b810c12d619393d8750ea670
programmergeek <programergeek@gmail.com>
2020-08-31 12:31:56 +0200

    Created Time component

---
+++ daily-starter/src/components/Time/Time.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Time extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Time
