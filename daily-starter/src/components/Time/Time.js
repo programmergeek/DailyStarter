f96c4720c70cf428f8e23916a6202c4070e908c8
programmergeek <programergeek@gmail.com>
2020-08-31 13:38:01 +0200

    created method setDate

--- daily-starter/src/components/Time/Time.js
+++ daily-starter/src/components/Time/Time.js
@@ -9,6 +9,15 @@ class Time extends React.Component{
         }
     }
 
+    setDate(){
+        //returns a formated string for the date
+        var date = new Date()
+        var year = date.getFullYear()
+        var month = date.getMonth()
+        var day = date.getDay()
+        return year+'/ '+(month+1)+'/ '+day
+    }
+
     render(){
         return(
             <div>
