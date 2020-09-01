3347fd2b3b31c8c71cf3ca0913a07df02059586e
programmergeek <programergeek@gmail.com>
2020-09-01 21:26:30 +0200

    created state city

--- daily-starter/src/components/Input/CityInput.js
+++ daily-starter/src/components/Input/CityInput.js
@@ -3,6 +3,9 @@ import React from 'react'
 class CityInput extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            city : ''
+        }
     }
 
 
