82b53832416f7d43dbb16670f5688a414dc973d8
programmergeek <programergeek@gmail.com>
2020-09-01 21:11:05 +0200

    removed state object from CityInput.js

--- daily-starter/src/components/Input/CityInput.js
+++ daily-starter/src/components/Input/CityInput.js
@@ -3,9 +3,6 @@ import React from 'react'
 class CityInput extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            city : ''
-        }
     }
 
 
