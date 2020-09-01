4660dfe9a26627d2764433c84fa5644ab394e922
programmergeek <programergeek@gmail.com>
2020-09-01 20:13:25 +0200

    created CityInput component

---
+++ daily-starter/src/components/Input/CityInput.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class CityInput extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default CityInput
