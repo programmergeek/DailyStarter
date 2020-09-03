139d8158c30c0106efbca3ff5047c49aec3e2b74
programmergeek <programergeek@gmail.com>
2020-09-03 17:07:14 +0200

    encapsulated news artcle in bootstrap cards

--- daily-starter/src/components/News/News.js
+++ daily-starter/src/components/News/News.js
@@ -76,16 +76,16 @@ class News extends React.Component{
         var list = []
         for (var x = 0; x < 5; x++){
             list.push(
-                <div>
-                    <img src={this.state.data.articles[x].urlToImage} style={{
-                        width: 150,
-                        height: 100
-                    }} alt="image"/>
-                    <h3 className="Headline"> {this.state.data.articles[x].title} </h3>
-                    <h5 className="author"> {this.state.data.articles[x].author} </h5>
-                    <p className="description" > {this.state.data.articles[x].description} </p>
-                    <a href={this.state.data.articles[x].url}>Source</a>
-                    
+                <div style={{margin: 20, width: '26rem'}} >
+                    <div class="card text-left">
+                      <img class="card-img-top" src={this.state.data.articles[x].urlToImage} alt="image"/>
+                      <div class="card-body">
+                        <h4 class="card-title"> {this.state.data.articles[x].title} </h4>
+                        <h6 class="card-subtitle mb-2 text-muted"> {this.state.data.articles[x].author} | {this.state.data.articles[x].source.name} </h6>
+                        <p class="card-text"> {this.state.data.articles[x].description} </p>
+                        <a href={this.state.data.articles[x].url} class="card-link"> Source </a>
+                      </div>
+                    </div>
                 </div>
             )
             console.log(list)
